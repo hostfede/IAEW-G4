bb15661ebdd24925bbd08cf54b5edeb116c66c47
EmmaGalizio <emmagalzio@gmail.com>
2023-11-23 11:29:46 -0300

    Documentacion swagger

--- src/main/java/iaew/tpi/apiClientes/restController/ClientesController.java
+++ src/main/java/iaew/tpi/apiClientes/restController/ClientesController.java
@@ -2,11 +2,18 @@ package iaew.tpi.apiClientes.restController;
 
 
 import iaew.tpi.apiClientes.controllers.*;
+import iaew.tpi.apiClientes.exceptions.ApiError;
 import iaew.tpi.apiClientes.reservas.clients.IClienteReservasGen;
 import iaew.tpi.apiClientes.reservas.transferObjects.ReservaAlojamientoDto;
 import iaew.tpi.apiClientes.transferObjects.ClienteDto;
 import iaew.tpi.apiClientes.transferObjects.ReservaWrapper;
 import iaew.tpi.apiClientes.transferObjects.ResponseWrapperDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
@@ -33,11 +40,25 @@ public class ClientesController {
     @Value("${baseUrl.reservasAlojamiento}")
     private String alojamientosBaseUrl;
 
+    @Operation(summary = "Recuperar un cliente por su id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Se encontró el cliente",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ClienteDto.class)) }),
+            @ApiResponse(responseCode = "404", description = "Cliente no encontrado",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ApiError.class)) })})
     @GetMapping("/{id_cliente}")
     public ResponseEntity<ClienteDto> getCliente(@PathVariable Integer id_cliente){
         return ResponseEntity.ok(getService.getCliente(id_cliente));
     }
 
+    @Operation(summary = "Recuperar lista de clientes")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Listado de clientes",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ResponseWrapperDto.class)) })
+            })
     @GetMapping
     public ResponseEntity<ResponseWrapperDto> getAll(
             @RequestParam(name = "page", required = false, defaultValue = "1") int page,
@@ -45,24 +66,63 @@ public class ClientesController {
         return ResponseEntity.ok(getService.getClientes(page, size));
     }
 
+    @Operation(summary = "Modificar cliente")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Cliente actualizado",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ClienteDto.class)) }),
+            @ApiResponse(responseCode = "404", description = "Cliente no encontrado",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ApiError.class)) }),
+            @ApiResponse(responseCode = "400", description = "Formato de solicitud incorrecto",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ApiError.class)) })})
     @PostMapping
     public ResponseEntity<ClienteDto> createCliente(@RequestBody ClienteDto clienteDto){
         return ResponseEntity.ok(postService.createCliente(clienteDto));
     }
 
+    @Operation(summary = "Modificar cliente")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Cliente actualizado",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ClienteDto.class)) }),
+            @ApiResponse(responseCode = "404", description = "Cliente no encontrado",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ApiError.class)) }),
+            @ApiResponse(responseCode = "400", description = "Formato de solicitud incorrecto",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ApiError.class)) })})
     @DeleteMapping("/{id_cliente}")
     public ResponseEntity<ClienteDto> deleteCliente(@PathVariable("id_cliente") Integer id_cliente){
         return ResponseEntity.ok(deleteService.deleteCliente(id_cliente));
     }
 
+    @Operation(summary = "Modificar cliente")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Cliente actualizado",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ClienteDto.class)) }),
+            @ApiResponse(responseCode = "404", description = "Cliente no encontrado",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ApiError.class)) }),
+            @ApiResponse(responseCode = "400", description = "Formato de solicitud incorrecto",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ApiError.class)) })})
     @PutMapping("/{id_cliente}")
     public ResponseEntity<ClienteDto> updateCliente(@PathVariable("id_cliente") Integer idCliente,@RequestBody ClienteDto clienteDto){
         return ResponseEntity.ok(putService.updateCliente(idCliente, clienteDto));
     }
 
-    /**
-     * Endpoint de prueba para el cliente genérico. No deberían hacer falta los otros clientes.
-     * */
+
+    @Operation(summary = "Recuperar un cliente por su id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Reservas de cliente encontradas",
+                    content = { @Content(mediaType = "application/json",
+                            array = @ArraySchema(schema = @Schema(implementation = ReservaWrapper.class))) }),
+            @ApiResponse(responseCode = "404", description = "Cliente o reservas no encontrado",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ApiError.class)) })})
     @GetMapping("/{id_cliente}/reservas")
     public ResponseEntity<List<ReservaWrapper>> getReservas(@PathVariable Integer id_cliente){
         List<ReservaWrapper> reservas = reservasService.getReservasCliente(id_cliente);
